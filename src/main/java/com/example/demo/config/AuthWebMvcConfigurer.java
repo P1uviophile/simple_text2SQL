ec481941e6a4fd041410f9f74951ab653f40c5de
P1uviophile <2083717185@qq.com>
2024-06-11 21:09:03 +0800

    添加配置注释

--- src/main/java/com/example/demo/config/AuthWebMvcConfigurer.java
+++ src/main/java/com/example/demo/config/AuthWebMvcConfigurer.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 配置拦截器
+ */
 @Configuration
 public class AuthWebMvcConfigurer implements WebMvcConfigurer {
     @Autowired
--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+/**
+ * 配置全局跨域
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
